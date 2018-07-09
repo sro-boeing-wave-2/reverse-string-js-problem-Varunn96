5b219f6bb0f15f37ad0488ff9d1aaa611dfd04ff
Varun Naidu <n.varunnaidu@gmail.com>
2018-07-09 15:26:54 +0530

    added code

--- reverse-using-built-in-function.js
+++ reverse-using-built-in-function.js
@@ -1,5 +1,10 @@
 // The intent of this file is to reverse
 // the string using built in functions.
-const reverse = () => {};
+const reverse = (inputstring) => {
+  var splitstring=inputstring.split('');
+  var revstring=splitstring.reverse();
+  var outputstring=revstring.join('');
+  return outputstring;
+};
 
 module.exports = reverse;
--- reverse-using-iterators.js
+++ reverse-using-iterators.js
@@ -1,4 +1,10 @@
 // The intent of this file is to reverse the string using iterators (for, foreach)
-const reverse = () => {};
+const reverse = (inputstring) => {
+  var outputstring='';
+  for(var i=inputstring.length;i>0;i--){
+    outputstring+=inputstring[i-1];
+  }
+  return outputstring;
+};
 
 module.exports = reverse;
